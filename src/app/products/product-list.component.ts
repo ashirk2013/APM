05ab144bc1405c5e62499317743cb055088249dc
Aaron Shirk <ashirk@jmt.com>
2020-02-28 18:02:09 -0500

    http method and observable

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
     showImage: boolean = false;
     products: IProduct[] = [];
     filteredProducts: IProduct[];
+    errorMessage: string;
 
     constructor(private productService: ProductService) {
     }
@@ -40,8 +41,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
     }
 
     onRatingClicked(message: string): void {
